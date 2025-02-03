96fcad1104c02765eff7effa4f0badd993a90e20
Abhishek Singh <abhishek.singh@shoogloo.com>
2025-02-03 17:15:30 +0530

    Commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <Head>
         {/* Font Awesome CDN */}
         <link

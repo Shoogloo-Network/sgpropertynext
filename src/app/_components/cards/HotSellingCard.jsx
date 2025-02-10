9d57a28098b32dd434bff3219da1f1526f7c8f7f
Abhishek Singh <abhishek.singh@shoogloo.com>
2025-02-10 15:15:05 +0530

    Clean up

--- src/app/_components/cards/HotSellingCard.jsx
+++ src/app/_components/cards/HotSellingCard.jsx
@@ -1,9 +1,8 @@
 import { useRouter } from 'next/navigation';
 import './HotSellingCard.css';
-import IconCard from './IconCard';
 import BtnPrimaryWithIcon from '../buttons/BtnPrimaryWithIcon';
 
-const HotSellingCard = ({ title, description, descriptionPrice, iconCardData, onClick, url }) => {
+const HotSellingCard = ({ title, description, descriptionPrice, onClick }) => {
     const router = useRouter();
 
     const onImageClick = () => {
@@ -16,29 +15,30 @@ const HotSellingCard = ({ title, description, descriptionPrice, iconCardData, on
             <div className="hotselling-card-content" onClick={onClick}>
                 <BtnPrimaryWithIcon iconName="FaPhone" cnt="Contact" />
             </div>
-            <div className="hotselling-card-content" onClick={onClick} style={{ display: "flex", flexDirection: "column", gap: "0", alignItems: "flex-start" }}>
-                <h3 className="hotselling-card-title">{title}</h3>
-                <div >
-                    {/* <IconCard data={iconCardData} /> */}
-                    <div style={{ display: "flex", alignItems: "center", gap: "2px" }}><img src='/images/building.png' alt='icon image' height={20} width={20} />
-                        <div>
-                            <p>Project Size</p>
-                            <p>165 unit, 1.25 Acres</p>
-                        </div>
-                    </div>
 
+            <div className="hotselling-card-content hotselling-flex-column" onClick={onClick}>
+                <h3 className="hotselling-card-title">{title}</h3>
+                <div className="hotselling-info-wrapper">
+                    <img src='/images/building.png' alt='icon image' className="hotselling-icon-image" />
+                    <div className="hotselling-info-wrapper-div">
+                        <p>Project Size</p>
+                        <p>165 unit, 1.25 Acres</p>
+                    </div>
                 </div>
             </div>
-            <div className="hotselling-card-content" onClick={onClick} style={{ display: "flex", flexDirection: "column", gap: "0", alignItems: "flex-start" }}>
+
+            <div className="hotselling-card-content hotselling-flex-column" onClick={onClick}>
                 <h3 className="hotselling-card-description">{description}</h3>
-                <div style={{ display: "flex", alignItems: "center", gap: "2px" }}><img src='/images/building.png' alt='icon image' height={20} width={20} />
-                    <div>
+                <div className="hotselling-info-wrapper">
+                    <img src='/images/building.png' alt='icon image' className="hotselling-icon-image" />
+                    <div className="hotselling-info-wrapper-div">
                         <p>Configuration</p>
                         <p>2-BHK, 3-BHK Flats</p>
                     </div>
                 </div>
             </div>
-            <div className="hotselling-card-content" onClick={onClick} style={{ display: "flex", flexDirection: "column", gap: "0", alignItems: "flex-start" }}>
+
+            <div className="hotselling-card-content hotselling-flex-column" onClick={onClick}>
                 <h3 className="hotselling-card-description">Price Range</h3>
                 <p className="hotselling-card-description-color">{descriptionPrice}</p>
             </div>

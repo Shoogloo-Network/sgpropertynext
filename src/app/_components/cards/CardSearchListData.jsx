3902ff7ad681c5ff0bab7364e4fd7e5b7a2af7f7
Abhishek Singh <abhishek.singh@shoogloo.com>
2025-02-10 17:54:19 +0530

    Commit

--- src/app/_components/cards/CardSearchListData.jsx
+++ src/app/_components/cards/CardSearchListData.jsx
@@ -10,7 +10,7 @@ import '../../agents/agent-details/AgentDetails.css'
 
 const CardSearchListData = memo(({ cities }) => {
   const [selectedFilter, setSelectedFilter] = useState("All");
-  const [selectedCity , setSelectedCity] = useState("");
+  const [selectedCity , setSelectedCity] = useState("Pune");
   const filters = ["All", "New Launch", "Ready To Move", "Upcoming"];
   const handleFilterClick = (filter) => {
     
